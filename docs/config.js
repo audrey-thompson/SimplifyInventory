774f8c3ccd92888a57b85cdf584b421321fd51aa
audrey-thompson <audrey.thompson2@snhu.edu>
2026-04-21 16:48:17 -0500

    Profile/Project site JS

---
+++ docs/config.js
@@ -0,0 +1,27 @@
+const PAGES = [
+  {
+    title: "Page One",
+    video: "",          
+    content: "content/page1.md",
+  },
+  {
+    title: "Page Two",
+    video: "",
+    content: "content/page2.md",
+  },
+  {
+    title: "Page Three",
+    video: "",
+    content: "content/page3.md",
+  },
+  {
+    title: "Page Four",
+    video: "",
+    content: "content/page4.md",
+  },
+  {
+    title: "Page Five",
+    video: "",
+    content: "content/page5.md",
+  },
+];
