548bfed85d2b8eedb4ca578ffe259c52b111329e
audrey-thompson <audrey.thompson2@snhu.edu>
2026-04-22 00:32:30 -0500

    Add video links to multiple pages in config.js

--- docs/config.js
+++ docs/config.js
@@ -6,22 +6,22 @@ const PAGES = [
   },
   {
     title: "Page Two",
-    video: "",
+    video: "https://www.youtube.com/watch?v=CFnw0UY_poI",
     content: "content/page2.md",
   },
   {
     title: "Page Three",
-    video: "",
+    video: "https://www.youtube.com/watch?v=EJsBkfhtqB8",
     content: "content/page3.md",
   },
   {
     title: "Page Four",
-    video: "",
+    video: "https://www.youtube.com/watch?v=afd4OyPNS28",
     content: "content/page4.md",
   },
   {
     title: "Page Five",
-    video: "",
+    video: "https://www.youtube.com/watch?v=J2WoRbXDNro",
     content: "content/page5.md",
   },
 ];
